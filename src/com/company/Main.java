f44ec03484fe0137aca42b32d8eadd05d01ddb94
Saint <danya.zakharow01@yandex.ru>
2020-09-05 11:03:36 +0300

    pr1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,19 +4,6 @@ public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("hello");
-        int a=31;
-        int b=21;
-        int c=10;
-        int d=10;
-        System.out.println("a + b = " +(a + b));
-        System.out.println("a - b = " +(a - b));
-        System.out.println("b - a = " +(b - a));
-        System.out.println("a++= "+(a++));
-        System.out.println("++a= "+(++a));
-        System.out.println("c==d = "+(c==d));
-        System.out.println("c!=a = "+(c!=a));
-        System.out.println("c>=d = "+(c>=d));
 
     }
 }
